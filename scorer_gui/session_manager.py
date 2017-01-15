d0ac67e1cba63d5fb593e33e85b5d9117a9cf761
Francesco Battaglia <fpbattaglia@gmail.com>
2017-01-15 22:30:40 +0100

    added stub for session manager

---
+++ scorer_gui/session_manager.py
@@ -0,0 +1,21 @@
+import numpy as np
+import pandas as pd
+
+class SessionManager:
+    def __init__(self, filename, initial_trial = 0):
+        # raises ValueError
+        pass
+
+    def get_scheme_trial_info(self):
+        # raises ValueError
+        pass
+
+    def set_trial_info(self, info):
+        # raises ValueError
+        pass
+
+    def get_video_file_name_for_trial(self):
+        pass
+
+    def set_event(self, ts, frame_no, msg):
+        pass
