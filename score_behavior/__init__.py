df9c433f0204ec0209adf405600e75ea8e225907
Francesco Battaglia <fpbattaglia@gmail.com>
2018-03-01 19:13:45 +0100

    cleaned up versioning mechanism

--- score_behavior/__init__.py
+++ score_behavior/__init__.py
@@ -1,5 +1,7 @@
 # Set default logging handler to avoid "No handler found" warnings.
 import logging
+import os
+import subprocess
 
 try:  # Python 2.7+
     from logging import NullHandler
@@ -7,6 +9,16 @@ except ImportError:
     class NullHandler(logging.Handler):
         def emit(self, record):
             pass
+
+
+current_path = os.getcwd()
+try:
+    GIT_VERSION = subprocess.check_output(["git", "describe"]).strip().decode('utf-8')
+    VERSION = GIT_VERSION.split('-')[0]
+except subprocess.CalledProcessError as e:
+    here = os.path.dirname(__file__)
+    with open(os.path.join(here, 'VERSION')) as f:
+        GIT_VERSION = f.read()
 
 appname = "Score"
 appauthor = "MemDynLab"
--- setup.py
+++ setup.py
@@ -8,14 +8,18 @@ import subprocess
 
 current_path = os.getcwd()
 try:
-    os.chdir(os.path.dirname(__file__))
     GIT_VERSION = subprocess.check_output(["git", "describe"]).strip().decode('utf-8')
-    GIT_VERSION = GIT_VERSION.split('-')[0]
+    VERSION = GIT_VERSION.split('-')[0]
 except subprocess.CalledProcessError as e:
     GIT_VERSION = "0.1"
+    VERSION = GIT_VERSION
+
 os.chdir(current_path)
 
-here = os.path.abspath(os.path.dirname(__file__))
+here = os.getcwd()
+
+with open(os.path.join(here, 'score_behavior', 'VERSION'), 'w') as f:
+    f.write(GIT_VERSION)
 
 # Get the long description from the README file
 with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
@@ -26,11 +30,12 @@ setup(description='Behavioral scorer',
       long_description=long_description,
       author='Francesco Battaglia',
       author_email='fpbattaglia@gmail.com',
-      version=GIT_VERSION,
+      version=VERSION,
       license='GPL',
       packages=['score_behavior', 'score_behavior.ObjectSpace', 'score_behavior.tracking',
                 'score_behavior.tracking.geometry', 'score_behavior.tracking_controller'],
-      package_data={'resources': ['score_behavior/resources/*.*']},
+      package_data={'resources': ['score_behavior/resources/*.*'],
+                    'score_behavior': 'VERSION'},
       include_package_data=True,
       name='score_behavior',
       entry_points="""
