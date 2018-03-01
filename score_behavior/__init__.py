9b011196ff3a2f5a1241cc3efdf890153212bcb6
Francesco Battaglia <fpbattaglia@gmail.com>
2018-03-01 10:27:35 +0100

    fixing versioning problem

--- score_behavior/__init__.py
+++ score_behavior/__init__.py
@@ -1,15 +1,5 @@
 # Set default logging handler to avoid "No handler found" warnings.
 import logging
-import os
-import subprocess
-
-current_path = os.getcwd()
-try:
-    os.chdir(os.path.dirname(__file__))
-    GIT_VERSION = subprocess.check_output(["git", "describe"]).strip().decode('utf-8')
-except subprocess.CalledProcessError as e:
-    GIT_VERSION = "Unknown"
-os.chdir(current_path)
 
 try:  # Python 2.7+
     from logging import NullHandler
--- setup.py
+++ setup.py
@@ -3,11 +3,30 @@ try:
 except ImportError:
     from distutils.core import setup
 
-# from score_behavior import GIT_VERSION
+import os
+import subprocess
+
+current_path = os.getcwd()
+try:
+    os.chdir(os.path.dirname(__file__))
+    GIT_VERSION = subprocess.check_output(["git", "describe"]).strip().decode('utf-8')
+    GIT_VERSION = GIT_VERSION.split('-')[0]
+except subprocess.CalledProcessError as e:
+    GIT_VERSION = "0.1"
+os.chdir(current_path)
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Get the long description from the README file
+with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 
 setup(description='Behavioral scorer',
+      long_description=long_description,
       author='Francesco Battaglia',
       author_email='fpbattaglia@gmail.com',
+      version=GIT_VERSION,
       license='GPL',
       packages=['score_behavior', 'score_behavior.ObjectSpace', 'score_behavior.tracking',
                 'score_behavior.tracking.geometry', 'score_behavior.tracking_controller'],
