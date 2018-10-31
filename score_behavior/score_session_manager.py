f75d57e5b57135421b351808162de2821dc625b6
Francesco Battaglia <fpbattaglia@gmail.com>
2018-10-31 13:50:16 +0100

    now comments for each trial can be concatenated

--- score_behavior/score_session_manager.py
+++ score_behavior/score_session_manager.py
@@ -268,7 +268,11 @@ This program will cowardly refuse to continue""".format(min_free_disk_space))
         self.cur_scheduled_run = i
 
     def set_comments(self, comments):
-        self.comments = comments
+        print("COMMENTS*"+comments+"*")
+        if len(comments) > 0:
+            self.comments = self.comments + '|' + comments
+        else:
+            self.comments = ''
 
     @staticmethod
     def make_datetime_string(d):
