560b1f27c596f42eff04ae0be73cfdbf56daf2f8
Francesco Battaglia <fpbattaglia@gmail.com>
2018-11-11 21:55:37 +0100

    setup script installs object png files

--- setup.py
+++ setup.py
@@ -5,6 +5,10 @@ except ImportError:
 
 import os
 import subprocess
+import glob
+import appdirs
+from score_behavior import appauthor, appname
+
 
 current_path = os.getcwd()
 try:
@@ -25,20 +29,23 @@ with open(os.path.join(here, 'score_behavior', 'VERSION'), 'w') as f:
 with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
+object_files = glob.glob('score_behavior/resources/objects/*')
+
+object_dir = os.path.join(appdirs.user_data_dir(appname, appauthor), 'objects')
 
 setup(description='Behavioral scorer',
       long_description=long_description,
       author='Francesco Battaglia',
       author_email='fpbattaglia@gmail.com',
       version=VERSION,
-      license='GPL',
+      license='GPLv3',
       packages=['score_behavior', 'score_behavior.ObjectSpace', 'score_behavior.tracking',
                 'score_behavior.tracking.geometry', 'score_behavior.tracking_controller'],
-      package_data={'resources': ['score_behavior/resources/*.*'],
-                    'score_behavior': 'VERSION'},
       include_package_data=True,
+      data_files=[(object_dir, object_files)],
       name='score_behavior',
       entry_points="""
         [console_scripts]
         score=score_behavior.score_window:_main
-      """, install_requires=['pandas', 'appdirs', 'neuroseries', 'pyqt', 'numpy'])
+      """)
+      #install_requires=['pandas', 'appdirs', 'neuroseries', 'PyQt5', 'numpy'])
